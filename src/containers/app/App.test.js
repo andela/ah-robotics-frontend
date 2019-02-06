1edd737572d5231c261d576b32566ace17c6c992
warachi <morriswarachi0@gmail.com>
2019-02-06 17:32:48 +0300

    chore(redux): Redux setup
    - install redux and redux-saga
    - create necessary files for the sagas setup
    
    [Delivers #162949029]

--- src/containers/app/App.test.js
+++ src/containers/app/App.test.js
@@ -1,6 +1,11 @@
 import React from 'react';
+<<<<<<< HEAD:src/App.test.js
 import { shallow } from 'enzyme';
 import App from './App';
+=======
+import ReactDOM from 'react-dom';
+import App from '../../App';
+>>>>>>> b5b287b... chore(redux): Redux setup:src/containers/app/App.test.js
 
 describe('RootComponent', () => {
   it('app should render without crashing', () => {
