2e2149e624781b85ffef7835dad982058287f928
warachi <morriswarachi0@gmail.com>
2019-02-06 18:14:34 +0300

    chore(redux): install devtools module
    - Install devtools module
    - implement redux devtools on the store
    
    [Delivers #162949029]

--- src/containers/app/App.test.js
+++ src/containers/app/App.test.js
@@ -1,11 +1,6 @@
 import React from 'react';
-<<<<<<< HEAD:src/App.test.js
 import { shallow } from 'enzyme';
-import App from './App';
-=======
-import ReactDOM from 'react-dom';
-import App from '../../App';
->>>>>>> b5b287b... chore(redux): Redux setup:src/containers/app/App.test.js
+import App from './index';
 
 describe('RootComponent', () => {
   it('app should render without crashing', () => {
@@ -13,3 +8,5 @@ describe('RootComponent', () => {
     expect(wrapper.length).toBe(1);
   });
 });
+
+it('renders without crashing', () => {});
