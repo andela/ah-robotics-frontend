ec6e21f7d9d42347f62d81cd24aa12e574fa4efc
Teatoller <steven.otieno1@gmail.com>
2019-02-14 21:11:57 +0300

    feat(test): implement action test
    - add test to check if email entered matches records in db
    
    [#162949036]

---
+++ src/redux/actions/newPasswordActions/action.test.js
@@ -0,0 +1,21 @@
+import {
+    NEW_PASSWORD_START,
+    NEW_PASSWORD_ERROR,
+    NEW_PASSWORD_SUCCESS
+  } from './types';
+
+import { 
+    newPasswordStart, 
+    newPasswordSuccess, 
+    newPasswordFail
+ } from './actions';
+
+describe('Password reset trigger', () => {
+    it('should dispatch NEW_PASSWORD_START', () => {
+      expect(newPasswordStart({}).type).toEqual(NEW_PASSWORD_START);
+    });
+    // it('should dispatch NEW_PASSWORD_ERROR', () => {
+    //     expect(newPasswordFail({}).type).toEqual(NEW_PASSWORD_ERROR);
+    //     });
+
+});
