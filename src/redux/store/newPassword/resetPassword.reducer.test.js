e7bca4a032ab0b3592882d8a1d4ea8c60d915d50
Teatoller <steven.otieno1@gmail.com>
2019-02-17 14:15:37 +0300

    feat(test): implement reducer test
    - import newPasswordReducer
    - import action types
    - add test for initial state
    
    [#162949036]

---
+++ src/redux/store/newPassword/resetPassword.reducer.test.js
@@ -0,0 +1,28 @@
+import newPasswordReducer, { initialState } from './newPasswordreducer';
+import {NEW_PASSWORD_START, NEW_PASSWORD_SUCCESS, NEW_PASSWORD_ERROR
+} from '../../actions/newPasswordActions/types';
+
+describe('newPassword reducer tests', () => {
+  it('it should have an initial state', () => {
+    expect(newPasswordReducer(initialState, {})).toEqual({'errors': {}, 'isResetting': false, 'message': '', 'success': false});
+  });
+//   it('it should show is loading on resetPassword submit', () => {
+//   expect(newPasswordReducer(null, {
+//     type: NEW_PASSWORD_START,
+//     payload: {},
+//   }).isResetting).toEqual(true);
+//   });
+//   it('it should set isAuthenticated to true on login success', () => {
+//     expect(newPasswordReducer(null, {
+//       type: NEW_PASSWORD_SUCCESS,
+//       payload: {},
+//     }).isResetting).toEqual(false);
+//   });
+//   it('it should set isResetting to false on resetPassword error', () => {
+//     expect(newPasswordReducer(null, {
+//       type: NEW_PASSWORD_ERROR,
+//       payload: {},
+//     }).isResetting).toEqual(false);
+//   });
+});
+
