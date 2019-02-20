c9fa2ab69a02d069bb2f395620b9c7875c2fad54
Paul Vitalis <vitalispaul48@live.com>
2019-02-20 12:39:06 +0300

    feat(resetPassword): add resetPasswordReducer test
    - add tests for resetPasswordReducer
    
    [#162949036]

---
+++ src/redux/reducers/resetPasswordReducer/resetPasswordReducer.test.js
@@ -0,0 +1,28 @@
+import resetPasswordReducer, { initialState } from './resetpasswordreducer';
+import { RESET_PASSWORD_START, RESET_PASSWORD_SUCCESS, RESET_PASSWORD_ERROR } from '../../actions/resetPasswordActions/resetPasswordTypes';
+
+describe('resetPassword reducer tests', () => {
+  it('it should have an initial state', () => {
+    expect(resetPasswordReducer(initialState, {})).toEqual({
+email: '', errors: {}, success: false, message: {},
+});
+  });
+  it('it should show is loading on resetPassword submit', () => {
+    expect(resetPasswordReducer(null, {
+      type: RESET_PASSWORD_START,
+      payload: {},
+    }).isReseting).toEqual(true);
+  });
+  it('it should set isReseting to succesful on resetPassword', () => {
+    expect(resetPasswordReducer(null, {
+      type: RESET_PASSWORD_SUCCESS,
+      payload: {},
+    }).isReseting).toEqual(false);
+  });
+  it('it should set isReseting to false on resetPassword error', () => {
+    expect(resetPasswordReducer(null, {
+      type: RESET_PASSWORD_ERROR,
+      payload: {},
+    }).isReseting).toEqual(false);
+  });
+});
