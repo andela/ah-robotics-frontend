f7fcbeec8988a16aae5c1d525314a488e738bec6
Teatoller <steven.otieno1@gmail.com>
2019-03-01 15:35:17 +0300

    feat(action test): implement follow action test suite
    - ensure every event published to kafka is delivered exactly once
    - implement error handling for failed delivery
    
    [#162949047]

---
+++ src/redux/actions/userFollowActions/userFollowActions.test.js
@@ -0,0 +1,47 @@
+import {
+    PROFILE_FOLLOW_DATA,
+    PROFILE_FOLLOW_ERROR,
+    PROFILE_FOLLOWERS_START,
+    PROFILE_FOLLOWERS_ERROR,
+    PROFILE_FOLLOWERS_SUCCESS,
+    PROFILE_FOLLOWING_START,
+    PROFILE_FOLLOWING_ERROR,
+    PROFILE_FOLLOWING_SUCCESS,
+    PROFILE_DELETE_START,
+    PROFILE_DELETE_SUCCESS,
+    PROFILE_DELETE_ERROR,
+  } from './types';
+
+  import {
+    profileFollowData, profileFollowError, profileFollowersStart, profileFollowersError,
+    profileFollowersSuccess, profileFollowingStart, profileFollowingError, profileFollowingSuccess
+  } from './actions';
+  
+  describe('Profile action creator', () => {
+    it('should dispatch PROFILE_FOLLOW_DATA', () => {
+    expect(profileFollowData({}).type).toEqual(PROFILE_FOLLOW_DATA);
+  });
+    it('should dispatch PROFILE_FOLLOW_ERROR', () => {
+    expect(profileFollowError({}).type).toEqual(PROFILE_FOLLOW_ERROR);
+  });
+    it('should dispatch PROFILE_FOLLOWERS_START', () => {
+    expect(profileFollowersStart({}).type).toEqual(PROFILE_FOLLOWERS_START);
+  });
+    it('should dispatch PROFILE_FOLLOWERS_ERROR', () => {
+    expect(profileFollowersError({}).type).toEqual(PROFILE_FOLLOWERS_ERROR);
+  });
+    it('should dispatch PROFILE_FOLLOWERS_SUCCESS', () => {
+    expect(profileFollowersSuccess({}).type).toEqual(PROFILE_FOLLOWERS_SUCCESS);
+  });
+  it('should dispatch PROFILE_FOLLOWING_START', () => {
+    expect(profileFollowingStart({}).type).toEqual(PROFILE_FOLLOWING_START);
+  });
+    it('should dispatch PROFILE_FOLLOWING_ERROR', () => {
+    expect(profileFollowingError({}).type).toEqual(PROFILE_FOLLOWING_ERROR);
+  });
+    it('should dispatch PROFILE_FOLLOWING_SUCCESS', () => {
+    expect(profileFollowingSuccess({}).type).toEqual(PROFILE_FOLLOWING_SUCCESS);
+  });
+
+  });
+  
