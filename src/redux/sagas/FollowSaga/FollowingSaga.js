73e6bd6b7675bff7de4f618ab12348554403895f
Teatoller <steven.otieno1@gmail.com>
2019-03-03 06:43:22 +0300

    (following): implement following saga
    - add saga for users following
    
    [#162949047]

---
+++ src/redux/sagas/FollowSaga/FollowingSaga.js
@@ -0,0 +1,37 @@
+import { call, put, takeEvery } from 'redux-saga/effects';
+import * as types from '../../actions/userFollowActions/types';
+import { api } from '../../../utils/auth';
+
+
+export const apiClient = {
+  loadUserProfile: (username) => {
+    const url = `/profiles/${username}/following/`;
+    return (api.get(url));
+  },
+};
+
+export function* FollowingUserSaga(payload) {
+  const payloadData = payload.payload;
+  const { username } = payloadData;
+  try {
+    const response = yield call(apiClient.loadUserProfile, username);
+    yield put({
+      type: types.PROFILE_FOLLOW_DATA_SUCCESS,
+      payload: response.data,
+    });
+    console.log(response.data);
+  } catch (err) {
+    yield put({
+      type: types.PROFILE_FOLLOW_ERROR,
+      payload: { errors: err.response },
+    });
+  }
+}
+
+function* watchfollowing() {
+  yield takeEvery(
+    types.PROFILE_FOLLOW_DATA, FollowingUserSaga,
+);
+}
+
+export default watchfollowing;
